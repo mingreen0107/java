797bcef8f0198e722d2216e33266633a6865b37c
mingreen0107 <71gnos@naver.com>
2023-04-25 00:06:01 +0900

    b

---
+++ Helloword/src/b1008.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class b1008 {
+
+	public static void main(String[] args) {
+		Scanner scan = new Scanner(System.in);
+		double A = scan.nextInt();
+		double B = scan.nextInt();
+		double C = A/B;
+		
+		System.out.println(C);
+		
+		scan.close();
+
+	}
+
+}
