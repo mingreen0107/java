3ca50d3e16ab619b21082b0d89a5def0f5ac4c59
mingreen0107 <71gnos@naver.com>
2023-04-24 23:59:12 +0900

    b

--- Helloword/src/b1001.java
+++ Helloword/src/b1001.java
@@ -1,17 +1,15 @@
 import java.util.Scanner;
 
-public class b1000 {
+public class b1001 {
 
 	public static void main(String[] args) {
-		
 		Scanner scan = new Scanner(System.in);
 		int A = scan.nextInt();
 		int B = scan.nextInt();
 		
-		System.out.println(A+B);
+		System.out.println(A-B);
 		
 		scan.close();
-
 	}
 
 }
