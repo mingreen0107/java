4d05c3bb4433b3244a498044c33dfcae54227809
mingreen0107 <71gnos@naver.com>
2023-04-25 12:49:19 +0900

    b

---
+++ Helloword/src/b1546.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+
+public class b1546 {
+
+	public static void main(String[] args) {
+		Scanner scan = new Scanner(System.in);
+		int N = scan.nextInt();
+		double[] score = new double[1000];
+		double max=-1;
+		double sum=0;
+		
+		for(int i=0; i<N; i++) {
+			score[i] = scan.nextInt();
+			
+			if(score[i]>=max)
+				max=score[i];
+		}
+		for(int i=0; i<N; i++) {
+			score[i]= (score[i]/max)*100;
+			sum+=score[i];
+		}
+		System.out.println(sum/N);
+	}
+
+}
