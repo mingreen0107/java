fb34cca22697f501e82a84d4c3c67a23d5cb87c2
mingreen0107 <71gnos@naver.com>
2023-04-25 13:04:13 +0900

    b

---
+++ Helloword/src/b2439.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class b2439 {
+
+	public static void main(String[] args) {
+		Scanner scan = new Scanner(System.in);
+		int N = scan.nextInt();
+		
+		for(int i=0; i<N; i++) {
+			for(int j=i+1; j<N; j++)
+				System.out.print(" ");
+			for(int k=N-i; k<=N; k++)
+				System.out.print("*");
+			System.out.println();
+		}
+
+	}
+
+}
